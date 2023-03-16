5120b1e1ad1c9d91219ff1b0edb866684c9c8638
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-03-16 10:15:00 +0200

    adding more pages

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
-import Sidenav from "./components/Sidenav";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Skills from "./components/Skills";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Home />
+      <About />
+      <Skills />
+      <Contact />
     </div>
   );
 }
--- vite-project/src/components/Home.tsx
+++ vite-project/src/components/Home.tsx
@@ -1,25 +1,29 @@
 import React from "react";
 import { HiArrowNarrowRight } from "react-icons/hi";
 import { TypeAnimation } from "react-type-animation";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import Jackson from "../assets/jackson.jpg";
 
 const Home: React.FC = () => {
   return (
-    <div className="w-full h-screen bg-[#e9eaeb]">
+    <div id="home" className="w-full h-screen bg-[#e9eaeb]">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-black-600 font-bold ml-1">Hi, my name is</p>
         <h1 className="text-4xl sm:text-7xl font-bold text-[#181819] mb-3">
           Loc Nguyen
         </h1>
-        <h2 className="flex text-4xl sm:text-7xl font-bold text-[#4b5465]">
-          I'm a
+        <h2 className="flex text-4xl sm:text-7xl font-bold text-[#de0495]">
+          I'm
           <TypeAnimation
             sequence={[
-              " Developer",
-              2000,
-              " Coder",
-              2000,
-              " Tech Enthusiast",
+              "a Developer",
+              1000,
+              "a Coder",
+              1500,
+              "a Tech Enthusiast",
               2000,
             ]}
             wrapper="div"
@@ -33,6 +37,33 @@ const Home: React.FC = () => {
           designing) exceptional digital experiences. Currently, I’m focused on
           building responsive full-stack web applications.
         </p>
+
+        <div className="flex items-center justify-between max-w-[330px] mb-5 mt-3 ">
+          <a
+            href="https://www.linkedin.com/in/loc-nguyen-30129a217/"
+            rel="noreferrer"
+          >
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaLinkedinIn />
+            </div>
+          </a>
+          <a href="https://github.com/Jackson1611" rel="noreferrer">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaGithub />
+            </div>
+          </a>
+          <a href="/#contact">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail />
+            </div>
+          </a>
+          <a href="/resume">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <BsFillPersonLinesFill />
+            </div>
+          </a>
+        </div>
+
         <div>
           <button className="text-black group border-2 border-black px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
             View Work
